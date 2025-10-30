15e95add3d04d428da5ca16bb4d4b5e7b66bfd16
qiguili <qigui.li@igg.com>
2025-10-30 21:57:16 +0800

    修改ndk 库

--- VideoPlayerPlugin/app/src/main/build_android.bat
+++ VideoPlayerPlugin/app/src/main/build_android.bat
@@ -1,3 +1,3 @@
 cd /d "%cd%\"
-D:/work_tool/NDK/ndk-build.cmd NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=jni/Android.mk NDK_APPLICATION_MK=jni/Application.mk -j4
+D:/work_tool/NDK23/ndk-build.cmd NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=jni/Android.mk NDK_APPLICATION_MK=jni/Application.mk -j4
 pause
--- VideoPlayerPlugin/app/src/main/obj/local/x86/objs/CorePlus/__/cpp/RenderingPlugin.o.d
+++ VideoPlayerPlugin/app/src/main/obj/local/x86/objs/CorePlus/__/cpp/RenderingPlugin.o.d
@@ -1,172 +1,172 @@
 ./obj/local/x86/objs/CorePlus/__/cpp/RenderingPlugin.o: \
   jni\..\cpp\RenderingPlugin.cpp \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\math.h \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\__config \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\vector \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\iosfwd \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\wchar.h \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\__bit_reference \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\bit \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\limits \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\type_traits \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\version \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\__debug \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\algorithm \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\initializer_list \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\cstddef \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\cstring \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\string.h \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\utility \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\__tuple \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\cstdint \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\stdint.h \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\memory \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\typeinfo \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\exception \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\cstdlib \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\stdlib.h \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\new \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\iterator \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\__functional_base \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\tuple \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\stdexcept \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\atomic \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\__threading_support \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\chrono \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\ctime \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\ratio \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\climits \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\limits.h \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\errno.h \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\functional \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\__split_buffer \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\math.h \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\__config \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\vector \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\iosfwd \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\wchar.h \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\__bit_reference \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\bit \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\limits \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\type_traits \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\version \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\__debug \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\algorithm \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\initializer_list \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\cstddef \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\cstring \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\string.h \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\utility \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\__tuple \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\cstdint \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\stdint.h \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\memory \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\typeinfo \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\exception \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\cstdlib \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\stdlib.h \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\new \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\iterator \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\__functional_base \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\tuple \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\stdexcept \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\atomic \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\__threading_support \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\chrono \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\ctime \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\ratio \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\climits \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\limits.h \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\errno.h \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\functional \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\__split_buffer \
   jni\..\cpp\Unity\IUnityGraphics.h jni\..\cpp\Unity\IUnityInterface.h \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\string \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\string_view \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\__string \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\cstdio \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\stdio.h \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\cwchar \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\cwctype \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\cctype \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\ctype.h \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\wctype.h \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\list \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\map \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\__tree \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\__node_handle \
-  D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\optional
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\string \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\string_view \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\__string \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\cstdio \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\stdio.h \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\cwchar \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\cwctype \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\cctype \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\ctype.h \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\wctype.h \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\list \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\map \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\__tree \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\__node_handle \
+  D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\optional
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\math.h:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\math.h:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\__config:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\__config:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\vector:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\vector:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\iosfwd:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\iosfwd:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\wchar.h:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\wchar.h:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\__bit_reference:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\__bit_reference:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\bit:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\bit:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\limits:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\limits:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\type_traits:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\type_traits:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\version:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\version:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\__debug:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\__debug:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\algorithm:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\algorithm:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\initializer_list:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\initializer_list:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\cstddef:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\cstddef:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\cstring:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\cstring:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\string.h:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\string.h:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\utility:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\utility:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\__tuple:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\__tuple:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\cstdint:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\cstdint:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\stdint.h:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\stdint.h:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\memory:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\memory:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\typeinfo:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\typeinfo:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\exception:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\exception:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\cstdlib:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\cstdlib:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\stdlib.h:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\stdlib.h:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\new:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\new:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\iterator:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\iterator:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\__functional_base:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\__functional_base:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\tuple:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\tuple:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\stdexcept:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\stdexcept:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\atomic:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\atomic:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\__threading_support:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\__threading_support:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\chrono:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\chrono:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\ctime:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\ctime:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\ratio:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\ratio:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\climits:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\climits:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\limits.h:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\limits.h:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\errno.h:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\errno.h:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\functional:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\functional:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\__split_buffer:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\__split_buffer:
 
 jni\..\cpp\Unity\IUnityGraphics.h:
 
 jni\..\cpp\Unity\IUnityInterface.h:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\string:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\string:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\string_view:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\string_view:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\__string:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\__string:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\cstdio:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\cstdio:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\stdio.h:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\stdio.h:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\cwchar:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\cwchar:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\cwctype:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\cwctype:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\cctype:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\cctype:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\ctype.h:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\ctype.h:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\wctype.h:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\wctype.h:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\list:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\list:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\map:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\map:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\__tree:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\__tree:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\__node_handle:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\__node_handle:
 
-D:\work_tool\NDK\build\\..\sources\cxx-stl\llvm-libc++\include\optional:
+D:\work_tool\NDK23\build\\..\sources\cxx-stl\llvm-libc++\include\optional:
